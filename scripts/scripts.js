f166b8be88dce481ae0b6a298df807545d8a0e01
dianaurtikova <dianaurtikova567@gmail.com>
2025-05-14 16:46:25 +0000

    js3

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,29 +1,29 @@
-const cards = {
-    card_1: {
-        card_foto: "img/card1.1.png",
-        card_name: "Portal TV",
-        card_description: "Smart video calling on the biggest screen in your home",
-        card_link: "Learn More",
-    },
-    card_2: {
-        card_foto: "img/card1.2.png",
-        card_name: "Portal",
-        card_description: "Smart video calling on a 10” HD display",
-        card_link: "Learn More",
-    },
-    card_3: {
-        card_foto: "img/card1.3.png",
-        card_name: "Portal+",
-        card_description: "Smart video calling on a 15.6”",
-        card_link: "Learn More",
-    },
-    card_4: {
-        card_foto: "img/card1.4.png",
-        card_name: "Portal Mini",
-        card_description: "Smart video calling on an 8” HD display”",
-        card_link: "Learn More",
-    },
-}
+// const cards = {
+//     card_1: {
+//         card_foto: "img/card1.1.png",
+//         card_name: "Portal TV",
+//         card_description: "Smart video calling on the biggest screen in your home",
+//         card_link: "Learn More",
+//     },
+//     card_2: {
+//         card_foto: "img/card1.2.png",
+//         card_name: "Portal",
+//         card_description: "Smart video calling on a 10” HD display",
+//         card_link: "Learn More",
+//     },
+//     card_3: {
+//         card_foto: "img/card1.3.png",
+//         card_name: "Portal+",
+//         card_description: "Smart video calling on a 15.6”",
+//         card_link: "Learn More",
+//     },
+//     card_4: {
+//         card_foto: "img/card1.4.png",
+//         card_name: "Portal Mini",
+//         card_description: "Smart video calling on an 8” HD display”",
+//         card_link: "Learn More",
+//     },
+// }
 function showCard(card) {
     let infCards = document.getElementById("information-cards");
     let newCard = `
@@ -44,7 +44,7 @@ function showAllCard(cards){
         showCard(cardArray[i]);
     }
 }
-showAllCard(cards);
+// showAllCard(cards);
 
 //2.1 слайдер
 const swiper1 = new Swiper('.swiper.swiper', {
@@ -66,4 +66,15 @@ function openBackdrop() {
   document.getElementsByClassName("backdrop")[0].classList.remove("off1");
   document.documentElement.style.overflow = "hidden"
 }
-  
+//3 задание
+fetch('https://jsonplaceholder.typicode.com/comments?_limit=4')
+  .then(response => response.json())
+  .then(json => {
+    const cards = json.map((comment, index) => ({
+      card_foto: "img/card1.1.png", 
+      card_name: `Comment ${index + 1}`,
+      card_description: comment.body,
+      card_link: 'Learn More',
+    }));
+    showAllCard(cards);
+  });
